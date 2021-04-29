4361ddb14f516e74b01ee489c87e325f9e793c57
Dmitry Tolstekov <dtolstienkov@gmail.com>
2021-04-29 18:35:01 +0300

    error fix 2

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -1,4 +1,4 @@
-package com.example.kalkulator
+package com.example.myapplication
 
 import android.content.Context
 import android.content.Intent
--- app/src/main/java/com/example/myapplication/RegisterActivity.kt
+++ app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -1,4 +1,4 @@
-package com.example.kalkulator
+package com.example.myapplication
 
 import android.app.Dialog
 import android.content.DialogInterface
